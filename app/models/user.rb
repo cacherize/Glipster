7b3a8c5a57e6dd103ffb4a8dd0effa1302981c94
unknown <web.developing.maniac@gmail.com>
2014-02-22 02:11:03 -0600

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password_digest
+  attr_accessible :email, :username, :password, :password_confirmation, :admin, :activated
+  has_secure_password
+
+  validates :password, presence: true, on: :create
+  validates :email, presence: true, uniqueness: true, on: :create
+  validates :username, presence: true, uniqueness: true, on: :create
 end
