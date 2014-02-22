83be434da6e497a5b62a7a53d3c897be18b4357f
unknown <web.developing.maniac@gmail.com>
2014-02-22 02:02:18 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glipster::Application.config.secret_token = '9f6190e8c0e0ebd26f0af6e9d0ec2f9629cce4a72e1b7e6f0acc4a256ba8b178bab281ada2af8a5d77853014d411748c6f835c63f6638cf69956f2e1c5b59d35'
