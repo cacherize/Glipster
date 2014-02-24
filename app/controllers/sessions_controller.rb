380330bf3c71658f4f3181f9ced56cc0b7652e82
unknown <web.developing.maniac@gmail.com>
2014-02-23 21:41:02 -0600

    don't allow access unless user is activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,14 @@ class SessionsController < ApplicationController
 	  	user = User.find_by_email_or_username(params[:username])
 	  end
   	if user && user.authenticate(params[:password])
-  		session[:user_id] = user.id
-  		redirect_to root_path, notice: 'Logged in!'
+  		if user.activated?
+	  		session[:user_id] = user.id
+	  		redirect_to root_path, notice: 'Logged in!'
+	  	else
+	  		redirect_to login_path, alert: 'Please check you inbox for a confirmation email to activate your account'
+	  	end
   	else
+  		flash.now.alert = "Invalid login credentials"
   		render 'new'
   	end
   end
