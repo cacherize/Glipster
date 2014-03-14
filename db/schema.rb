896aad76b3fc27ba1bb5e864c277de2d72988f15
cacherize <web.developing.maniac@gmail.com>
2014-03-13 20:53:37 -0500

    added category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140310015746) do
+ActiveRecord::Schema.define(:version => 20140314015155) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email"
