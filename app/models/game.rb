930d5e8a3fa6d2d2872d3efb8649431e21953d39
cacherize <web.developing.maniac@gmail.com>
2014-03-14 23:49:52 -0500

    added file name method for game

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,8 @@
 class Game < ActiveRecord::Base
   attr_accessible :title, :description, :controls, :play_count, :flash_file
   mount_uploader :flash_file, GameUploader
+
+  def flash_file_name
+  	File.basename(flash_file.path || flash_file.filename) if flash_file
+  end
 end
