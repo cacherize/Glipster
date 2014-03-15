fe3e4dd81341fc9ca86aa1eeb3280c461b38efed
cacherize <web.developing.maniac@gmail.com>
2014-03-14 23:05:23 -0500

    added game new view and controller action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,17 @@
 class GamesController < ApplicationController
+	def new
+		@game = Game.new
+	end
+
+	def create
+		@game = Game.new(param[:id])
+
+		respond_to do |format|
+			if @game.save
+				format.html{redirect_to @game}
+			else
+				format.html{render :new}
+			end
+		end
+	end
 end
