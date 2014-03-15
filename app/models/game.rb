77f96ebf78393ee2ba347a63cb598d0a0a8de679
cacherize <web.developing.maniac@gmail.com>
2014-03-14 23:21:26 -0500

    mount uploader on game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ActiveRecord::Base
-  attr_accessible :title, :description, :controls, :play_count
+  attr_accessible :title, :description, :controls, :play_count, :flash_file
+  mount_uploader :flash_file, GameUploader
 end
