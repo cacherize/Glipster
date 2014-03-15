c1e9d2f9272b37b5ab920be5ca9fad46a03733cf
cacherize <web.developing.maniac@gmail.com>
2014-03-15 02:35:13 -0500

    added alt tag in user and game helper image methods

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,9 +1,9 @@
 module GamesHelper
 	def game_thumb(game)
 		if game.image.present?
-			image_tag(game.image.thumb('75x75#').url)
+			image_tag(game.image.thumb('75x75#').url, alt: "#{game.title} image")
 		else
-			image_tag('default_game.png')
+			image_tag('default_game.png', alt: "#{game.title} image")
 		end
 	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,17 +1,17 @@
 module UsersHelper
 	def avatar_thumb(user)
 		if user.image.present?
-			image_tag(user.image.thumb('30x30#').url)
+			image_tag(user.image.thumb('30x30#').url, alt: "#{user.username} image")
 		else
-			image_tag('thumb_default_avatar.png')
+			image_tag('thumb_default_avatar.png', alt: "#{user.username} image")
 		end
 	end
 
 	def avatar_large(user)
 		if user.image.present?
-			image_tag(user.image.thumb('150x150#').url)
+			image_tag(user.image.thumb('150x150#').url, alt: "#{user.username} image")
 		else
-			image_tag('large_default_avatar.png')
+			image_tag('large_default_avatar.png', alt: "#{user.username} image")
 		end
 	end
 end
