40be8aaa98d1d012f5266e3bf72835a8055b39cc
cacherize <web.developing.maniac@gmail.com>
2014-03-15 00:07:59 -0500

    added dragonfly accessor to game model and image attribute to attr accessible

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,7 @@
 class Game < ActiveRecord::Base
-  attr_accessible :title, :description, :controls, :play_count, :flash_file
+  attr_accessible :title, :description, :controls, :play_count, :flash_file, :image
   mount_uploader :flash_file, GameUploader
+  dragonfly_accessor :image
 
   def flash_file_name
   	File.basename(flash_file.path || flash_file.filename) if flash_file
