d9d9aec1f0e041ae540908249344ba413f8bb311
cacherize <web.developing.maniac@gmail.com>
2014-03-15 01:18:01 -0500

    Set amazon s3 storage variables

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,4 +1,4 @@
-require 'dragonfly'
+require 'dragonfly/s3_data_store'
 
 # Configure
 Dragonfly.app.configure do
@@ -9,9 +9,14 @@ Dragonfly.app.configure do
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
+  # if Rails.env.test? || Rails.env.development?
+	  # datastore :file,
+	  #   root_path: Rails.root.join('public/system/dragonfly', Rails.env),
+	  #   server_root: Rails.root.join('public')
+	datastore :s3,
+    bucket_name: ENV["AWS_THUMBS_BUCKET"],
+    access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+    secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
 end
 
 # Logger
