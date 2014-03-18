97a714244660ac0b8d481e66bfbc5bed60fb62ba
cacherize <web.developing.maniac@gmail.com>
2014-03-17 20:33:22 -0500

    set up variables in developers controller

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -1,2 +1,50 @@
 class DevelopersController < ApplicationController
+	def new
+		@developer = Developer.new
+	end
+
+	def create
+		@developer = Developer.new(params[:developer])
+
+		respond_to do |format|
+			if @developer.save
+				format.html{redirect_to @developer, notice: "Successfully created developer!"}
+			else 
+				format.html{render :new}
+			end
+		end
+	end
+
+	def index
+		@developers = Developer.all
+	end
+
+	def show
+		@developer = Developer.find(params[:id])
+	end
+
+	def edit
+		@developer = Developer.find(params[:id])
+	end
+
+	def update
+		@developer = Developer.find(params[:id])
+
+		respond_to do |format|
+			if @developer.update_attributes(params[:developer])
+				format.html{redirect_to :index, notice: "Successfully updated developer!"}
+			else
+				format.html{render :edit}
+			end
+	end
+
+	def destroy
+		@developer = Developer.find(params[:id])
+		
+		@developer.update_attribute(:archived_at, Time.now)
+
+		respond_to do |format|
+			format.html{redirect_to :back}
+		end
+	end
 end
