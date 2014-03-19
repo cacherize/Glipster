f1eb1bae93f109d87f9de34bfb3af49ece2cecad
cacherize <web.developing.maniac@gmail.com>
2014-03-18 21:28:57 -0500

    added attributes to attr_accessible, association, and presence validations for game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,11 @@
 class Game < ActiveRecord::Base
-  attr_accessible :title, :description, :controls, :play_count, :flash_file, :image
+  attr_accessible :title, :description, :controls, :play_count, :flash_file, :image, :developer_id, :height, :width
   mount_uploader :flash_file, GameUploader
   dragonfly_accessor :image
+
+  belongs_to :developer
+
+  validates_presence_of :height, :width, :title
 
   def to_param
   	"#{id} #{title}".parameterize
