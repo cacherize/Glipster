b2454bf05b3758b677cf7050e32771f7e0a3ddd6
cacherize <web.developing.maniac@gmail.com>
2014-03-18 21:26:18 -0500

    added edit/update to games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,10 +18,26 @@ class GamesController < ApplicationController
 
 	def show
 		@game = Game.find(params[:id])
+		@developer = @game.developer
 	end
 
 	def upload
 		@uploader = Game.new.flash_file
 		@uploader.success_action_redirect = new_game_url
+	end
+
+	def edit 
+		@game = Game.find(params[:id])
+	end
+
+	def update
+		@game = Game.find(params[:id])
+		respond_to do |format|
+			if @game.update_attributes(params[:game])
+				format.html{redirect_to @game, notice: 'Successfully updated game!'}
+			else
+				format.html{render :edit}
+			end
+		end
 	end
 end
