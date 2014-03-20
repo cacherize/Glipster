e8bc3e1dddd4d028c65cc4643414abf59b19442d
cacherize <web.developing.maniac@gmail.com>
2014-03-19 21:56:50 -0500

    moves game list to partial

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
   	@games = Game.find(:all)
-  	@random_games = Game.offset(rand(Game.count)).last(12)
+  	@random_games = Game.offset(rand(Game.count)) #.last(12)
   end
 end
