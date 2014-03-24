b25dabb21ca608b71a5aa9ea874e41fd4b42e3f0
Micah <lazerdaser@gmail.com>
2014-03-23 20:13:53 -0500

    added authorize method in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,10 @@ class ApplicationController < ActionController::Base
   def current_user
   	@current_user ||= User.find_by_auth_token!(cookies[:auth_token]) if cookies[:auth_token]
   end
+
+  def authorize
+		redirect_to login_path, alert: 'Please log in to complete that action' unless current_user.present?  
+  end
+
   helper_method :current_user
 end
