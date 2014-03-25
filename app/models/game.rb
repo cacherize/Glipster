e48896301bad65e04a9e1c0cf688ff4b0e780c58
Micah <lazerdaser@gmail.com>
2014-03-24 22:11:56 -0500

    added game and categories assocations

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,7 @@ class Game < ActiveRecord::Base
   dragonfly_accessor :image
 
   belongs_to :developer
+  has_many :categories, through: :game_categories
   has_many :reputations
 
   validates_presence_of :height, :width, :title
