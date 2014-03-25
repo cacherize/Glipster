ec29ab8aec3eb016aa22e7ba96085c2b7fb2c74b
Micah <lazerdaser@gmail.com>
2014-03-24 22:04:01 -0500

    add show page for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,10 @@
 class CategoriesController < ApplicationController
+	def show
+		@category = Category.find(params[:id])
+
+		@games = @category.games
+	end
+
 	def new
 		@category = Category.new
 	end
