48f4da76b1736b69fa45b9dd9bb0ac2439cd2ea3
Micah <lazerdaser@gmail.com>
2014-03-26 22:55:31 -0500

    set comment's user and redirect comment's game

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,11 @@
 class CommentsController < ApplicationController
 	def create
 		@comment = Comment.new(params[:comment])
+		@comment.user_id = current_user.id
 		@comment.save
 
 		respond_to do |format|
-			format.js
+			format.html {redirect_to @comment.game}
 		end			
 	end
 end
