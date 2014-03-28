8d25c179cac0ba939007cb4018fdb627759c7738
Micah <lazerdaser@gmail.com>
2014-03-28 00:07:08 -0500

    paginate with ajax

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -8,3 +8,16 @@ function game_show() {
 	var width = $("#game_show").width();
 	$("#game_stats, #game_details, #comments_wrapper").width(width);
 }
+
+$(function(){
+	$("#comments_wrapper").on('click', '.pagination a', function(){
+		$(".pagination").html("<image src='/assets/spinner.gif'/>");
+		$.getScript(this.href)
+		return false;
+		// game_show();
+	});
+	// For older jQuery versions...
+	// jQuery.ajaxSetup({ 
+	//   'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+	// });
+});
