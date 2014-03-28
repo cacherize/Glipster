119a302996cded02f126b7270066847177ef9829
Micah <lazerdaser@gmail.com>
2014-03-27 19:33:59 -0500

    added authorize before_filter to comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
+	before_filter :authorize
 	def create
 		@comment = Comment.new(params[:comment])
 		@comment.user_id = current_user.id
 		@comment.save
 
 		respond_to do |format|
-			format.html {redirect_to @comment.game}
+			format.html {redirect_to @comment.game, notice: "Successfully submitted comment!"}
 		end			
 	end
 end
