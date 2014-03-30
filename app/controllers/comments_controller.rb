5d81343b84661eedd4f8d386ba766064e2356e9f
Micah <lazerdaser@gmail.com>
2014-03-30 17:50:08 -0500

    allow user to delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,4 +9,16 @@ class CommentsController < ApplicationController
 			format.html {redirect_to @comment.game, notice: "Successfully submitted comment!"}
 		end			
 	end
+
+	def destroy
+		@comment = Comment.find(params[:id])
+
+		respond_to do |format|
+			if @comment.destroy
+				format.html{redirect_to @comment.game, notice: "Successfully removed comment!"}
+			else
+				format.html{redirect_to @comment.game, alert: "Unable to delete comment, please try again."}
+			end
+		end
+	end
 end
