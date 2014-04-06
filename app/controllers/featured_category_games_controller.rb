fae897e54f287005c9527e6337b03016bd44e3b0
Micah <lazerdaser@gmail.com>
2014-04-05 23:15:47 -0500

    create/destroy actions for featured_category_games

--- app/controllers/featured_category_games_controller.rb
+++ app/controllers/featured_category_games_controller.rb
@@ -6,7 +6,26 @@ class FeaturedCategoryGamesController < ApplicationController
 
 	def show
 		@category = Category.find(params[:id])
-		@games = @category.games
+		@games = @category.games.order('created_at desc')
 		@featured_games = @category.featured_category_games
+	end
+
+	def create
+		@featured_game = FeaturedCategoryGame.new(params[:object])
+		respond_to do |format|
+			if @featured_game.save
+				format.html{redirect_to featured_category_game_path(@featured_game.category), notice: 'Successfully saved!'}
+			else
+				format.html{redirect_to featured_category_game_path(@featured_game.category), alert: 'Unable to save.'}
+			end
+		end
+	end
+
+	def destroy
+		@featured_game = FeaturedCategoryGame.find(params[:id])
+		@featured_game.destroy
+		respond_to do |format|
+			format.html{redirect_to featured_category_game_path(@featured_game.category), notice: 'Successfully deleted!'}
+		end
 	end
 end
