fb8686a6ca9d1a226165117ad43f3c4a678c3ea3
Micah <lazerdaser@gmail.com>
2014-04-05 23:14:53 -0500

    added uniqueness validation

--- app/models/featured_category_game.rb
+++ app/models/featured_category_game.rb
@@ -2,4 +2,5 @@ class FeaturedCategoryGame < ActiveRecord::Base
   attr_accessible :game_id, :category_id
   belongs_to :category
   belongs_to :game
+  validates_uniqueness_of :game_id, scope: :category_id
 end
