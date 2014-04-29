a9b8c1b647075bdf0692100611225a75c7aa0b8a
cacherize <web.developing.maniac@gmail.com>
2014-04-28 20:27:05 -0500

    added indent

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,9 +20,9 @@ $(document).ready(function(){
 	$('input, textarea').placeholder();
 
 	$('li.submenu_container').hover(function () {
-	if ($('> ul.submenu',this).length > 0) {
-	    $('> ul.submenu',this).stop().slideDown('fast');
-	}
+  	if ($('> ul.submenu',this).length > 0) {
+  	    $('> ul.submenu',this).stop().slideDown('fast');
+  	}
 	},function () {
 		if ($('> ul.submenu',this).length > 0) {
 			$('> ul.submenu',this).stop().slideUp('fast');
