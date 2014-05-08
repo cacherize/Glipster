7743bdfe1aa3430e9a78b674f3adbf856175f36b
cacherize <web.developing.maniac@gmail.com>
2014-05-07 21:28:26 -0500

    added character count

--- app/assets/javascripts/support_tickets.js
+++ app/assets/javascripts/support_tickets.js
@@ -6,5 +6,16 @@ $(document).ready(function() {
   });
   $("[name='support_ticket[reason]']").change(function(){
     $("#support_ticket_other_reason").val('');
-  })
+  });
+
+  $('#support_ticket_message').keyup(function () {
+    var max = 1000;
+    var len = $(this).val().length;
+    if (len >= max) {
+      $('#messageCharCount').text('No characters left');
+    } else {
+      var char = max - len;
+      $('#messageCharCount').text(char + ' characters left');
+    }
+  });
 });
