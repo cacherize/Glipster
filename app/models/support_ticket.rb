a8c94d2a04ee9e15f9d03a110d269f9067204ec9
cacherize <web.developing.maniac@gmail.com>
2014-05-07 20:09:05 -0500

    added :other_reason attr_accessible

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -1,5 +1,5 @@
 class SupportTicket < ActiveRecord::Base
-  attr_accessible :email, :reason, :message, :stickied, :archive
+  attr_accessible :email, :reason, :message, :stickied, :archive, :other_reason
 
   def archive
     self.archived_at
