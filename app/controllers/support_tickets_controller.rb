daf630e67f7215ab31deda9f9f4a183153bb2f1a
cacherize <web.developing.maniac@gmail.com>
2014-05-07 20:39:36 -0500

    added create action for support tickets

--- app/controllers/support_tickets_controller.rb
+++ app/controllers/support_tickets_controller.rb
@@ -2,4 +2,16 @@ class SupportTicketsController < ApplicationController
   def new
     @support_ticket = SupportTicket.new
   end
+
+  def create
+    @support_ticket = SupportTicket.new(params[:support_ticket])
+
+    respond_to do |format|
+      if @support_ticket.save
+        format.html{redirect_to root_path, notice: @support_ticket.success_message}
+      else
+        format.html{render :new}
+      end
+    end
+  end
 end
--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -8,4 +8,16 @@ class SupportTicket < ActiveRecord::Base
   def archive=(boolean)
     raise boolean.inspect
   end
+
+  def success_message
+    reason = self.reason
+    case reason
+    when ""
+    when "trouble registering"; "We're sorry that you're having trouble registering. Please wait for a reply on any further action."
+    when "making a suggestion"; "Thank you for your suggestion! Staff will review it."
+    when "report bug";          "Thanks for the bug report! Our staff will take time to review it."
+    else
+      "We got your message! A response will be delivered if necessary."
+    end
+  end
 end
