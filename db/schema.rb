af94a9bd21d5a9a03d436056782a6f1285e95b46
cacherize <web.developing.maniac@gmail.com>
2014-05-08 00:09:34 -0500

    added index to users username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140508022941) do
+ActiveRecord::Schema.define(:version => 20140508050753) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -125,5 +125,7 @@ ActiveRecord::Schema.define(:version => 20140508022941) do
     t.datetime "password_reset_sent_at"
     t.string   "image_uid"
   end
+
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
