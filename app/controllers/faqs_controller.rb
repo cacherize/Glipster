76e8277fcecdbe24f2a73f9ac04ff4cb1509f413
cacherize <web.developing.maniac@gmail.com>
2014-05-13 21:54:49 -0500

    added form to add FAQs

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -1,8 +1,18 @@
 class FaqsController < ApplicationController
   def index
+    @new_faq = Faq.new
   end
 
   def create
+    @new_faq = Faq.new(params[:faq])
+
+    respond_to do |format|
+      if @new_faq.save
+        format.html{redirect_to faq_path, notice: "Successfully added new FAQ!"}
+      else
+        format.html{render :index}
+      end
+    end
   end
 
   def update
