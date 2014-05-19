2dbf537ea021d746320c4f375f4ef6de275e7a58
cacherize <web.developing.maniac@gmail.com>
2014-05-18 23:13:57 -0500

    added user_id column to uploads

---
+++ db/migrate/20140519041254_add_user_id_column_to_uploads.rb
@@ -0,0 +1,5 @@
+class AddUserIdColumnToUploads < ActiveRecord::Migration
+  def change
+    add_column :uploads, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140515042026) do
+ActiveRecord::Schema.define(:version => 20140519041254) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -126,6 +126,7 @@ ActiveRecord::Schema.define(:version => 20140515042026) do
     t.string   "note",       :limit => 2048
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
