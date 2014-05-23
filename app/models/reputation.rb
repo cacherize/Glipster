f81a3d06812a449b1b9ea3e4b2485099a9bf25aa
cacherize <web.developing.maniac@gmail.com>
2014-05-22 23:09:56 -0500

    added reputation scopes

--- app/models/reputation.rb
+++ app/models/reputation.rb
@@ -2,4 +2,6 @@ class Reputation < ActiveRecord::Base
   attr_accessible :user_id, :game_id, :value
   belongs_to :user
   belongs_to :game
+  default_scope order("created_at DESC")
+  scope :positive_reps, where("value = ?", 1)
 end
