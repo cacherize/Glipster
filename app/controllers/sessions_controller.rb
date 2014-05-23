a3f8e33031038336a801793a69f6b7bf11e85e22
cacherize <web.developing.maniac@gmail.com>
2014-05-22 22:21:57 -0500

    limit login attempts

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    fail_tracker = FailedLogin.find_by_requester(request.remote_ip)
+
+    if !fail_tracker.nil? && fail_tracker.reached_attempts_limit
+      timer = fail_tracker.attempted_at - 8.minutes.ago
+      redirect_to(login_path, alert: "You have too many failed login attempts. Please try again in #{(timer/60).ceil} minutes.")
+      return
+    end
+
   	if params[:login].present?
 	  	user = User.find_by_email_or_username(params[:login])
 	  end
@@ -15,12 +23,18 @@ class SessionsController < ApplicationController
 	  		else
 	  			cookies[:auth_token] = user.auth_token
 	  		end
+        fail_tracker.destroy unless fail_tracker.nil?
 	  		redirect_back_or_default('/')
         flash[:notice] = "Logged in!"
 	  	else
 	  		redirect_to login_path, alert: 'Please check you inbox for a confirmation email to activate your account'
 	  	end
   	else
+      if fail_tracker.nil?
+        FailedLogin.create(requester: request.remote_ip, attempts: 1, attempted_at: DateTime.now)
+      else
+        fail_tracker.increment!(:attempts, 1)
+      end
   		flash.now.alert = "Invalid login credentials"
   		render 'new'
   	end
--- app/models/failed_login.rb
+++ app/models/failed_login.rb
@@ -1,3 +1,15 @@
 class FailedLogin < ActiveRecord::Base
   attr_accessible :requester, :attempts, :attempted_at
+
+  def reached_attempts_limit
+    if self.attempted_at > 8.minutes.ago
+      if attempts > 14
+        true
+      else
+        false
+      end
+    else
+      !self.destroy
+    end
+  end
 end
