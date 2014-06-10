6bfa18dd66f54dbae9518e33f16a33509e08fedb
cacherize <web.developing.maniac@gmail.com>
2014-06-09 22:06:34 -0500

    added comment_like model associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ActiveRecord::Base
   attr_accessible :game_id, :message
+ 
   belongs_to :game
   belongs_to :user
+  has_many :comment_likes
 
   validates_presence_of :user_id, :game_id
   default_scope {order('created_at desc')}
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,7 @@ class User < ActiveRecord::Base
   has_many :reputations
   has_many :game_users
   has_many :games, through: :game_users
+  has_many :comment_likes
 
   validates :password, presence: {message: 'must be provided'}, on: :create
   validates :email, presence: {message: 'must be provided'}, uniqueness: true, on: :create
