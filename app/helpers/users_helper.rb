46fa905d01559bb2d2d90bc097d9451987a1a9a2
cacherize <web.developing.maniac@gmail.com>
2014-06-10 22:32:16 -0500

    changed avatar image cropping

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 	def avatar_thumb(user)
 		if user.image.present?
-			image_tag(user.image.thumb('30x30#').url, alt: "#{user.username} image", class: 'u_img')
+			image_tag(user.image.thumb('30x').url, alt: "#{user.username} image", class: 'u_img')
 		else
 			image_tag('thumb_default_avatar.png', alt: "#{user.username} image", class: 'u_img')
 		end
@@ -9,7 +9,7 @@ module UsersHelper
 
 	def avatar_large(user, id)
 		if user.image.present?
-			image_tag(user.image.thumb('150x150#').url, alt: "#{user.username} image", id: id)
+			image_tag(user.image.thumb('150x').url, alt: "#{user.username} image", id: id)
 		else
 			image_tag('large_default_avatar.png', alt: "#{user.username} image", id: id)
 		end
