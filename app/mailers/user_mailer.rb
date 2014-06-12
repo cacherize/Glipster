73fbbfefc880bada2417cf8255eee7c6a14b7931
cacherize <web.developing.maniac@gmail.com>
2014-06-11 22:47:55 -0500

    added inline logo attachment for user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,12 @@
 class UserMailer < ActionMailer::Base
   default from: "no-reply@glipster.com"
 
+  unless self.included_modules.include?(AbstractController::Callbacks)
+    include AbstractController::Callbacks
+  end
+  
+  before_filter :add_inline_attachments!
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
@@ -14,5 +20,11 @@ class UserMailer < ActionMailer::Base
   def password_reset(user)
     @user = user
     mail to: user.email, subject: "Reset Password Instructions"
+  end
+
+  private
+ 
+  def add_inline_attachments!
+    attachments.inline["logo.jpg"] = File.read("#{Rails.root}/app/assets/images/email_logo.png")
   end
 end
