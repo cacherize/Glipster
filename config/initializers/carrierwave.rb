85a8e3342b3af962d61dda9475c6b90f6ef4582d
cacherize <web.developing.maniac@gmail.com>
2014-06-14 21:52:13 -0500

    set max file size to 50 mb on carrierwave uploads

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,4 +5,5 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],
   }
   config.fog_directory = ENV["AWS_BUCKET"]
+  config.max_file_size = 50.megabytes
 end
