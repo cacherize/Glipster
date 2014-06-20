f76354369d9ec69156ad340ad76a127110e62a25
cacherize <web.developing.maniac@gmail.com>
2014-06-19 23:14:55 -0500

    added form to submit sequel positions

--- app/controllers/game_sequels_controller.rb
+++ app/controllers/game_sequels_controller.rb
@@ -6,4 +6,16 @@ class GameSequelsController < ApplicationController
       format.js
     end
   end
+
+  def update
+    @game = Game.find(params[:id])
+    params[:positions].each do |k,v|
+      sequel = GameSequel.find(k)
+      sequel.update_attribute(:position, v)
+    end
+
+    respond_to do |format|
+      format.html{redirect_to @game, notice: "Successfully updated sequel positions!"}
+    end
+  end
 end
