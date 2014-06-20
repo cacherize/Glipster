c5a62a5a60b59c8d90c506e747978d1446672f87
cacherize <web.developing.maniac@gmail.com>
2014-06-19 23:27:25 -0500

    added default scope for game sequel position order

--- app/models/game_sequel.rb
+++ app/models/game_sequel.rb
@@ -2,4 +2,5 @@ class GameSequel < ActiveRecord::Base
   attr_accessible :game_id, :sequel_id
   belongs_to :game
   belongs_to :sequel, class_name: 'Game'
+  default_scope order('position ASC')
 end
