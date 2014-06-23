d20cc4e0ded4ecd9483da312e23dd4fa2ab93b9f
cacherize <web.developing.maniac@gmail.com>
2014-06-22 22:08:21 -0500

    grab first new games instead of last

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
   	@featured = Game.where(featured: true)
-  	@new_games = Game.order('id DESC').last(12)
+  	@new_games = Game.order('id DESC').first(12)
   	@random_game = [Game.first(:offset => rand(Game.count))]
     @images = FeaturedImage.all
   end
