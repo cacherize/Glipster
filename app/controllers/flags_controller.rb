bc82c7c4af87a049e7f65b4d360630f52bfe1297
cacherize <web.developing.maniac@gmail.com>
2014-06-25 20:43:34 -0500

    create flag with remote link on game show

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,2 +1,11 @@
 class FlagsController < ApplicationController
+  def create
+    @flag = Flag.new(user_id: current_user.id, game_id: params[:id])
+
+    respond_to do |format|
+      if @flag.save
+        format.js
+      end
+    end
+  end
 end
