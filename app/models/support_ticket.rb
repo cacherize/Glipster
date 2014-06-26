df52f903261dddb81d562f39319f3608d31604fa
cacherize <web.developing.maniac@gmail.com>
2014-06-26 18:14:53 -0500

    added unarchived scope for support tickets

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -10,6 +10,8 @@ class SupportTicket < ActiveRecord::Base
   belongs_to :game
   belongs_to :user
 
+  scope :unarchived, where("archived_at IS NOT NULL")
+
   def check_existing_tickets(ip)
     existing_ticket = SupportTicket.where(requester: ip).order("created_at ASC").last
     
