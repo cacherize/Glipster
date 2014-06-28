69841ac671b0ff3bf865c3302f40f4b186730b78
cacherize <web.developing.maniac@gmail.com>
2014-06-27 19:13:45 -0500

    added helper method to mark support ticket reason filters

--- app/helpers/support_tickets_helper.rb
+++ app/helpers/support_tickets_helper.rb
@@ -1,2 +1,7 @@
 module SupportTicketsHelper
+  def mark_support_ticket_reason_filters(reason, filters)
+    if filters[:reason].present?
+      filters[:reason].include?(reason)
+    end
+  end
 end
