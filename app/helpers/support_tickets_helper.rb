6f363e09d0426ca21f99dddc444900593ab351ee
cacherize <web.developing.maniac@gmail.com>
2014-06-27 19:17:02 -0500

    check if filters are present before checking if reason filters are present on mark_support_ticket_reason_filters method

--- app/helpers/support_tickets_helper.rb
+++ app/helpers/support_tickets_helper.rb
@@ -1,6 +1,6 @@
 module SupportTicketsHelper
   def mark_support_ticket_reason_filters(reason, filters)
-    if filters[:reason].present?
+    if filters.present? && filters[:reason].present?
       filters[:reason].include?(reason)
     end
   end
